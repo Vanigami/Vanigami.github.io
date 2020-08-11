4a05295c63876b1de4ad4b125b88c605198c2ae6
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-08-11 15:32:20 -0400

    add images for projects cards and fix styling typo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,20 +2,21 @@ import React, { Component } from "react";
 import ProjectCard from "./ProjectCard";
 import img1 from "../images/tsimg.png";
 import img2 from "../images/wcimg.png";
+import img3 from "../images/fspaimg.png";
 
 class Projects extends Component {
   render() {
     return (
       <div className="container-fluid d-flex justify-content-center">
         <div className="row">
-          <div calssName="col-md-4">
+          <div className="col-md-4">
             <ProjectCard imgsrc={img1} title="Training Session" />
           </div>
-          <div calssName="col-md-4">
+          <div className="col-md-4">
             <ProjectCard imgsrc={img2} title="Wine Cellar" />
           </div>
           <div className="col-md-4">
-            <ProjectCard />
+            <ProjectCard imgsrc={img3} title="Fitness-SPA" />
           </div>
         </div>
       </div>
