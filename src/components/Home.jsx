acd730d0bd69930723397c18df145a8df3a2552f
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-08-11 11:06:04 -0400

    add project 1 image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
+import Projects from "./Projects";
 import "../App.css";
 
 export default class Home extends Component {
   render() {
     return (
-      <div className="html">
-        <h1>Future Portfolio Website.</h1> <br />
-        <h2>Under Construction</h2>
-      </div>
+      <>
+        <div className="html">
+          <h1>Future Portfolio Website.</h1> <br />
+          <h2>Under Construction</h2>
+        </div>
+        <Projects />
+      </>
     );
   }
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,20 @@
+import Carousel from "react-bootstrap/Carousel";
+import React from "react";
+
+function Projects() {
+  // {
+  //   name: "Training Session",
+  //   url: ".imagesTrainingSessionCarousel.png",
+  // },
+  return (
+    <>
+      <h2>OICUTRE</h2>
+      <img
+        src="https://cdn.cp.adobe.io/content/2/dcx/d0117f96-de80-4c2b-b743-33ced6f6c8e4/rendition/preview.jpg/version/0/format/jpg/dimension/width/size/1200"
+        alt="Training Session"
+      />
+    </>
+  );
+}
+
+export default Projects;
