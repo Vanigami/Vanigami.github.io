5531f1bdc377029b6bc4ef96eb38347874c04e0d
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-08-11 15:37:14 -0400

    adds small about blurb

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,13 @@ class Home extends React.Component {
     return (
       <div>
         <h2 className="brandFont">Vanna Winland</h2>
+        <p className="brandFont">
+          Hi, my name is Vanna. I'm an English major turned software engineer.{" "}
+          <br />
+          I never thought I'd be sharing stories through the languages of the
+          web. <br />
+          But here you are.
+        </p>
         <Projects />
       </div>
     );
