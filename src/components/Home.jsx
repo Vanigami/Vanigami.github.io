7f5cfaa8b9063603c8676b11de46e65ce85f71ec
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-08-12 21:12:56 -0400

    add ruby png and animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import ProjectCard from "./ProjectCard";
 import Projects from "./Projects";
+import rubyLogo from "../images/ruby.png";
 import "../App.css";
+
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <li class="tab readme">
-          <i class="fas fa-info-circle"></i>READ.me
-        </li>
-
-        <h2 class="animate_animated animate__fadeInLeft">Vanna Winland</h2>
+        <h2 class="main-header ">Vanna Winland</h2>
         <p className="brandFont">
           Hi, my name is Vanna. I'm an English major turned software engineer.{" "}
           <br />
@@ -19,13 +17,50 @@ class Home extends React.Component {
           but here you are.
         </p>
         <Projects />
-        <div class="container">
-          <span class="react-logo">
-            <span class="nucleo"></span>
-          </span>
-        </div>
 
-        <p class="title">React</p>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Languages</th>
+              <th scope="col">Frameworks/Databases</th>
+              <th scope="col">Deployment</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>
+                <div class="container">
+                  <span class="react-logo">
+                    <span class="nucleo"></span>
+                  </span>
+                </div>
+
+                <p class="title">React</p>
+              </td>
+              <td>
+                <div>
+                  <span>
+                    <img class="ruby-logo" src={rubyLogo} alt="ruby-logo" />
+                  </span>
+                </div>
+              </td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
