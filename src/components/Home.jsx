01dd7b08eea03a2493d26f970dec3436330485a2
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-08-13 18:41:54 -0400

    change background

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,57 +17,15 @@ class Home extends React.Component {
           but here you are.
         </p>
         <Projects />
-        <p>
-          <a
-            class="btn btn-primary"
-            data-toggle="collapse"
-            href="#multiCollapseExample1"
-            role="button"
-            aria-expanded="false"
-            aria-controls="multiCollapseExample1"
-          >
-            Toggle first element
-          </a>
-          <button
-            class="btn btn-primary"
-            type="button"
-            data-toggle="collapse"
-            data-target="#multiCollapseExample2"
-            aria-expanded="false"
-            aria-controls="multiCollapseExample2"
-          >
-            Toggle second element
-          </button>
-          <button
-            class="btn btn-primary"
-            type="button"
-            data-toggle="collapse"
-            data-target=".multi-collapse"
-            aria-expanded="false"
-            aria-controls="multiCollapseExample1 multiCollapseExample2"
-          >
-            Toggle both elements
-          </button>
-        </p>
-        <div class="row">
-          <div class="col">
-            <div class="collapse multi-collapse" id="multiCollapseExample1">
-              <div class="card card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life
-                accusamus terry richardson ad squid. Nihil anim keffiyeh
-                helvetica, craft beer labore wes anderson cred nesciunt sapiente
-                ea proident.
-              </div>
+
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <i class="fab fa-angellist"></i>
             </div>
-          </div>
-          <div class="col">
-            <div class="collapse multi-collapse" id="multiCollapseExample2">
-              <div class="card card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life
-                accusamus terry richardson ad squid. Nihil anim keffiyeh
-                helvetica, craft beer labore wes anderson cred nesciunt sapiente
-                ea proident.
-              </div>
+            <div class="flip-card-back">
+              <h1>WEE </h1>
+              <p>Cool</p>
             </div>
           </div>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,18 +7,74 @@ import img3 from "../images/fspaimg.png";
 class Projects extends Component {
   render() {
     return (
-      <div className="container-fluid d-flex justify-content-center">
-        <div className="row">
-          <div className="col-md-4">
-            <ProjectCard imgsrc={img1} title="Training Session" />
+      <div>
+        <p>
+          <a
+            class="btn btn-primary"
+            data-toggle="collapse"
+            href="#multiCollapseExample1"
+            role="button"
+            aria-expanded="false"
+            aria-controls="multiCollapseExample1"
+          >
+            Toggle first element
+          </a>
+          <button
+            class="btn btn-primary"
+            type="button"
+            data-toggle="collapse"
+            data-target="#multiCollapseExample2"
+            aria-expanded="false"
+            aria-controls="multiCollapseExample2"
+          >
+            Toggle second element
+          </button>
+          <button
+            class="btn btn-primary"
+            type="button"
+            data-toggle="collapse"
+            data-target=".multi-collapse"
+            aria-expanded="false"
+            aria-controls="multiCollapseExample1 multiCollapseExample2"
+          >
+            Toggle both elements
+          </button>
+        </p>
+        <div class="row">
+          <div class="col">
+            <div class="collapse multi-collapse" id="multiCollapseExample1">
+              <div className="container-fluid d-flex justify-content-center">
+                <div className="row">
+                  <div className="col-md-4">
+                    <ProjectCard imgsrc={img1} title="Training Session" />
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="collapse multi-collapse" id="multiCollapseExample2">
+                  <div class="card card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid. Nihil anim keffiyeh
+                    helvetica, craft beer labore wes anderson cred nesciunt
+                    sapiente ea proident.
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
+          //{" "}
           <div className="col-md-4">
-            <ProjectCard imgsrc={img2} title="Wine Cellar" />
+            // <ProjectCard imgsrc={img2} title="Wine Cellar" />
+            //{" "}
           </div>
+          //{" "}
           <div className="col-md-4">
-            <ProjectCard imgsrc={img3} title="Fitness-SPA" />
+            // <ProjectCard imgsrc={img3} title="Fitness-SPA" />
+            //{" "}
           </div>
+          //{" "}
         </div>
+        //{" "}
       </div>
     );
   }
