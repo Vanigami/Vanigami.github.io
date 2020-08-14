ccb5610c51a20f55590bd0d9034a522861098f35
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-08-13 21:16:14 -0400

    add collapse plugin for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,72 +9,33 @@ class Projects extends Component {
     return (
       <div>
         <p>
-          <a
-            class="btn btn-primary"
-            data-toggle="collapse"
-            href="#multiCollapseExample1"
-            role="button"
-            aria-expanded="false"
-            aria-controls="multiCollapseExample1"
-          >
-            Toggle first element
-          </a>
           <button
             class="btn btn-primary"
             type="button"
             data-toggle="collapse"
-            data-target="#multiCollapseExample2"
+            data-target="#collapseExample"
             aria-expanded="false"
-            aria-controls="multiCollapseExample2"
+            aria-controls="collapseExample"
           >
-            Toggle second element
-          </button>
-          <button
-            class="btn btn-primary"
-            type="button"
-            data-toggle="collapse"
-            data-target=".multi-collapse"
-            aria-expanded="false"
-            aria-controls="multiCollapseExample1 multiCollapseExample2"
-          >
-            Toggle both elements
+            <i class="fas fa-project-diagram"></i>
+            <text className="brandFont">Projects</text>
           </button>
         </p>
-        <div class="row">
-          <div class="col">
-            <div class="collapse multi-collapse" id="multiCollapseExample1">
-              <div className="container-fluid d-flex justify-content-center">
-                <div className="row">
-                  <div className="col-md-4">
-                    <ProjectCard imgsrc={img1} title="Training Session" />
-                  </div>
-                </div>
+        <div class="collapse" id="collapseExample">
+          <div className="container-fluid d-flex justify-content-center">
+            <div className="row">
+              <div className="col-md-4">
+                <ProjectCard imgsrc={img1} title="Training Session" />
               </div>
-              <div class="col">
-                <div class="collapse multi-collapse" id="multiCollapseExample2">
-                  <div class="card card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life
-                    accusamus terry richardson ad squid. Nihil anim keffiyeh
-                    helvetica, craft beer labore wes anderson cred nesciunt
-                    sapiente ea proident.
-                  </div>
-                </div>
+              <div className="col-md-4">
+                <ProjectCard imgsrc={img2} title="Wine Cellar" />
+              </div>
+              <div className="col-md-4">
+                <ProjectCard imgsrc={img3} title="Fitness-SPA" />
               </div>
             </div>
           </div>
-          //{" "}
-          <div className="col-md-4">
-            // <ProjectCard imgsrc={img2} title="Wine Cellar" />
-            //{" "}
-          </div>
-          //{" "}
-          <div className="col-md-4">
-            // <ProjectCard imgsrc={img3} title="Fitness-SPA" />
-            //{" "}
-          </div>
-          //{" "}
         </div>
-        //{" "}
       </div>
     );
   }
