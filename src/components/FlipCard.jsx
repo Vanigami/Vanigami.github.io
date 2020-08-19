30f927a5b9975d7b19165222c72dfe96f7cfac94
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-08-18 23:30:43 -0400

    resize flip-card on home page

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -59,24 +59,9 @@ export default class FlipCard extends Component {
                   <span>NodeJS </span>
                   <img src={nodeJSLogo} />
                 </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="flip-card">
-          <div class="flip-card-inner">
-            <div class="flip-card-front">
-              <i class="fab fa-angellist"></i>
-            </div>
-            <div class="flip-card-back">
-              <ul>
-                <span>Ruby </span>
-                <img src={rubyLogo} /> <br />
-                <span>JavaScript </span>
-                <i class="fab fa-js"></i> <br />
-                <span>Libraries + Frameworks</span>
                 <br />
-                <img src={RoRLogo} />
+                <span class="flipHeading">Libraries + Frameworks</span>
+                <br />
               </ul>
             </div>
           </div>
