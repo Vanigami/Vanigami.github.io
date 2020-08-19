f4b0ca7f62955c673f4bbb68ed45040e3117ef33
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-08-19 11:44:00 -0400

    add carousel to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,46 +7,51 @@ import img3 from "../images/fspaimg.png";
 class Projects extends Component {
   render() {
     return (
-      <div>
-        <p>
-          <button
-            class="buttonProject"
-            type="button"
-            data-toggle="collapse"
-            data-target="#collapseExample"
-            aria-expanded="false"
-            aria-controls="collapseExample"
-          >
-            <i class="fas fa-project-diagram"></i>
-            <text className="brandFont">Projects</text>
-          </button>
-        </p>
-        <div class="collapse" id="collapseExample">
-          <div className="container-fluid d-flex justify-content-center">
-            <div className="row">
-              <div className="col-md-4">
-                <ProjectCard
-                  imgsrc={img1}
-                  title="Training Session"
-                  link="/TrainingSession"
-                />
-              </div>
-              <div className="col-md-4">
-                <ProjectCard
-                  imgsrc={img2}
-                  title="Wine Cellar"
-                  link="/WineCellar"
-                />
-              </div>
-              <div className="col-md-4">
-                <ProjectCard
-                  imgsrc={img3}
-                  title="Fitness-SPA"
-                  link="Fitness-SPA"
-                />
-              </div>
+      <div class="carousel">
+        <div
+          id="carouselExampleControls"
+          class="carousel slide"
+          data-ride="carousel"
+        >
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <a href="/Fitness-SPA">
+                <img class="carousel" src={img3} alt="First slide" />
+              </a>
+            </div>
+            <div class="carousel-item">
+              <a href="/WineCellar">
+                <img class="d-block " src={img2} alt="Second slide" />
+              </a>
+            </div>
+            <div class="carousel-item">
+              <a href="/TrainingSession">
+                <img class="d-block" src={img1} alt="Third slide" />
+              </a>
             </div>
           </div>
+          <a
+            class="carousel-control-prev black"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="prev"
+          >
+            <span
+              class="carousel-control-prev-icon"
+              aria-hidden="true"
+              color="black"
+            ></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a
+            class="carousel-control-next"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
       </div>
     );
