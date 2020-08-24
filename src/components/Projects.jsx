93bcd06851512cb27664f0b8b5dd12706bc83920
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-08-23 20:35:08 -0400

    get rid of carousel add a grid for projects!

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,55 +9,15 @@ import img3 from "../images/fspaimg.png";
 class Projects extends Component {
   render() {
     return (
-      <div class="carousel-container">
-        <div class="carousel">
-          <div
-            id="carouselExampleControls"
-            class="carousel slide"
-            data-ride="carousel"
-          >
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <Link to="/Fitness-SPA">
-                  <img class="carousel" src={img3} alt="First slide" />
-                </Link>
-              </div>
-              <div class="carousel-item">
-                <Link to="/WineCellar">
-                  <img class="d-block " src={img2} alt="Second slide" />
-                </Link>
-              </div>
-              <div class="carousel-item">
-                <Link to="/TrainingSession">
-                  <img class="d-block" src={img1} alt="Third slide" />
-                </Link>
-              </div>
-            </div>
-            <a
-              class="carousel-control-prev black"
-              href="#carouselExampleControls"
-              role="button"
-              data-slide="prev"
-            >
-              <span
-                class="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a
-              class="carousel-control-next"
-              href="#carouselExampleControls"
-              role="button"
-              data-slide="next"
-            >
-              <span
-                class="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
+      <div class="grid-container">
+        <div class="grid-item">
+          <img class="grid-img" src={img1}></img>
+        </div>
+        <div class="grid-item">
+          <img class="grid-img" src={img2}></img>
+        </div>
+        <div class="grid-item">
+          <img class="grid-img" src={img3}></img>
         </div>
       </div>
     );
